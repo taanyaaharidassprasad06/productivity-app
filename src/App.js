451c93c0e9b673e8e5803d5f2ca6e7c869ba6bd3
taanyaahp <taanyaahp@gmail.com>
2026-03-17 15:28:35 -0700

    working on sidebar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import TaskList from './components/TaskList';
 import { CategoryProvider } from './components/CategoryContext';
 import { TaskProvider } from './components/TaskContext';
+import { useState } from 'react';
+import Sidebar from './components/Sidebar';
 
 function App() {
+  const [sidebar, setSidebarOpen] = useState(false);
   return (
     <div className="base-container">
       <CategoryProvider>
         <TaskProvider>
-          <div>
+          <div className="main-section">
+            <button onClick={() => setSidebarOpen(!sidebar)}>#</button>
+            {sidebar && <Sidebar/>}
             <TaskList />
           </div>
         </TaskProvider>
---
+++ src/components/Sidebar.js
@@ -0,0 +1,10 @@
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h4>Navigation</h4>
+            <button>+ New List</button>
+        </div>
+    );
+}
+
+export default Sidebar;
